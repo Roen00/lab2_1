290a81194e5a3235dad896869db78d861e05486a
Rafal Zelek <r.zelek@oberthur.com>
2016-12-17 17:42:12 +0100

    added test class for search result object, which checks if isFound returns true if position is 0

--- src/test/java/edu/iis/mto/bsearch/SearchResultTest.java
+++ src/test/java/edu/iis/mto/bsearch/SearchResultTest.java
@@ -1,5 +1,16 @@
 package edu.iis.mto.bsearch;
 
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class SearchResultTest {
 
+    @Test
+    public void isFoundShouldReturnTrueForPosition0(){
+        final SearchResult sut = new SearchResult();
+        sut.setPosition(0);
+        assertThat(sut.isFound(), is(true));
+    }
 }
