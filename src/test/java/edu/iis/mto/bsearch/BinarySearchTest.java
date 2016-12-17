fdaaf24cb52c4a4ceade2e694ea8d22387325f2e
Rafal Zelek <r.zelek@oberthur.com>
2016-12-17 15:32:54 +0100

    implemented first case from test cases table

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -1,6 +1,18 @@
 package edu.iis.mto.bsearch;
 
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
 
 public class BinarySearchTest {
-
+    @Test
+    public void elementIsPresentedInSequenceAndSequenceLengthEqualsOne(){
+        final int searchedElement = 1;
+        final int[] seq = {searchedElement};
+        final SearchResult searchResult = BinarySearch.search(searchedElement, seq);
+        assertThat(searchResult.isFound(), is(true));
+        assertThat(searchedElement, is(seq[searchResult.getPosition()]));
+    }
+    
 }
